a53e6fb58f644d81ceaa7463f8d6f557127d87c9
BaxtComm <BaxtComm@BaxtComm-PC.attlocal.net>
2015-07-30 15:02:58 -0700

    Made A Key Bindings Thing That Doesn't Work Yet

--- src/keyPresses/KeyBindings.java
+++ src/keyPresses/KeyBindings.java
@@ -6,14 +6,18 @@ import javax.swing.KeyStroke;
 public class KeyBindings extends JComponent
 {
 	MoveLeft moveLeft;
+	MoveRight moveRight;
 
 	public KeyBindings()
 	{
 		moveLeft = new MoveLeft();
+		moveRight = new MoveRight();
 		
 		getInputMap().put(KeyStroke.getKeyStroke("A"), "MoveLeft");
+		getInputMap().put(KeyStroke.getKeyStroke("D"), "MoveRight");
 		
 		getActionMap().put("MoveLeft", moveLeft);
+		getActionMap().put("MoveRight", moveRight);
 	}
 	
 	
